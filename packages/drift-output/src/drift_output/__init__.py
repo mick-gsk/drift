57fcc4874fd905daa6cce8c9c73c4b878d45210d
mick-gsk <mick.gottsch@gmail.com>
2026-05-03 19:36:25 +0200

    refactor: migrate output helpers to drift-output package (ADR-100 phase 4c)

---
+++ packages/drift-output/src/drift_output/__init__.py
@@ -0,0 +1,42 @@
+"""Output formatters for Drift."""
+
+from drift_output import agent_tasks
+from drift_output.agent_tasks import analysis_to_agent_tasks_json
+from drift_output.csv_output import analysis_to_csv
+from drift_output.github_format import findings_to_github_annotations
+from drift_output.guided_output import (
+    TrafficLight,
+    can_continue,
+    determine_status,
+    emoji_for_status,
+    headline_for_status,
+    plain_text_for_signal,
+)
+from drift_output.json_output import analysis_to_json, findings_to_sarif
+from drift_output.prompt_generator import generate_agent_prompt
+from drift_output.rich_output import (
+    render_full_report,
+    render_recommendations,
+    render_timeline,
+    render_trend_chart,
+)
+
+__all__ = [
+    "TrafficLight",
+    "agent_tasks",
+    "analysis_to_agent_tasks_json",
+    "analysis_to_csv",
+    "analysis_to_json",
+    "can_continue",
+    "determine_status",
+    "emoji_for_status",
+    "findings_to_github_annotations",
+    "findings_to_sarif",
+    "generate_agent_prompt",
+    "headline_for_status",
+    "plain_text_for_signal",
+    "render_full_report",
+    "render_recommendations",
+    "render_timeline",
+    "render_trend_chart",
+]
